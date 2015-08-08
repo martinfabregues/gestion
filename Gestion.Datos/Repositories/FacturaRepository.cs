0b417b02a90e96e1eef39cce4ad4dda3ea92dd54
martinfabregues <martinfabregues@hotmail.com>
2015-08-08 13:58:13 -0300

    m

---
+++ Gestion.Datos/Repositories/FacturaRepository.cs
@@ -0,0 +1,73 @@
+﻿using Gestion.Datos.Interfases;
+using Gestion.Entidad;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DapperExtensions;
+
+namespace Gestion.Datos.Repositories
+{
+    public interface IFacturaRepository : IRepository<Factura>
+    {
+
+    }
+
+    public class FacturaRepository : IFacturaRepository
+    {
+
+        protected readonly IDbConnection _db;
+
+        public FacturaRepository()
+        {
+            try
+            {
+                _db = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ToString());
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+
+        public IEnumerable<Factura> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Add(Factura entity)
+        {
+            string query = "INSERT INTO FACTURA (FECHA, NUMERO, TIPOCOMPROBANTE_ID, PUNTOVENTA_ID, CONDICIONIVA_ID, CLIENTE_ID, CONCEPTO, SUBTOTAL, " +
+                            "IVA_27, IVA_21, IVA_105, IVA_5, IVA_25, OTROS_TRIBUTOS, TOTAL, OBSERVACIONES, CAE, FECHA_VENCIMIENTO_CAE, CODIGO_BARRAS, " +
+                            "ESTADO) VALUES ();" +
+                            "SELECT SCOPE_IDENTITY();";
+            try
+            {
+                using (IDbConnection _db = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ToString()))
+                {
+                    
+                }
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public int Modify(Factura entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gestion.Entidad/Factura.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion.Entidad
+{
+    public class Factura
+    {
+        public int id { get; set; }
+        public DateTime fecha { get; set; }
+        public string numero { get; set; }
+        public int tipocomprobante_id { get; set; }
+        public int puntoventa_id { get; set; }
+        public int condicionventa_id { get; set; }
+        public int cliente_id { get; set; }
+        public int concepto { get; set; }
+        public double subtotal { get; set; }
+        public double iva_27 { get; set; }
+        public double iva_21 { get; set;}
+        public double iva_105 { get; set; }
+        public double iva_5 { get; set; }
+        public double iva_25 { get; set; }
+        public double otros_tributos { get; set; }
+        public double total { get; set; }
+        public string observaciones { get; set; }
+        public string cae { get; set; }
+        public DateTime fecha_vencimiento_cae { get; set; }
+        public string codigo_barras { get; set; }
+        public string estado { get; set; }
+    }
+}
