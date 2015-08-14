f9dba6483c08c6c6f329b335fdfe69e7e22f2465
martinfabregues <martinfabregues@hotmail.com>
2015-08-14 10:35:06 -0300

    m

---
+++ Gestion.Entidad/FacturaAlicuota.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion.Entidad
+{
+    public class FacturaAlicuota
+    {
+        public int id { get; set; }
+        public int factura_id { get; set; }
+        public int alicuota_id { get; set; }
+        public double base_imponible { get; set; }
+        public double importe { get; set; }
+
+        public virtual Factura factura { get; set; }
+        public virtual Alicuota alicuota { get; set; }
+    }
+}
