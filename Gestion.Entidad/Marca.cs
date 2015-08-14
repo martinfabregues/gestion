c7a07d156092e857705838b2937b7e78d81d43c8
martinfabregues <martinfabregues@hotmail.com>
2015-08-13 21:31:54 -0300

    m

--- Gestion.Entidad/Marca.cs
+++ Gestion.Entidad/Marca.cs
@@ -9,6 +9,7 @@ namespace Gestion.Entidad
     public class Marca
     {
         public int id { get; set; }
+        public int proveedor_id { get; set; }
         public string marca { get; set; }
         public int activo { get; set; }
     }
