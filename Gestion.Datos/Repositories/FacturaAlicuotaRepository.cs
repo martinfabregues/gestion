4f75d5bb0854ff36f221e9a4f622a3e0bef7c5c5
martinfabregues <martinfabregues@hotmail.com>
2015-08-14 10:42:19 -0300

    m

---
+++ Gestion.Datos/Repositories/FacturaAlicuotaRepository.cs
@@ -0,0 +1,80 @@
+﻿using Gestion.Datos.Interfases;
+using Gestion.Entidad;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DapperExtensions;
+
+namespace Gestion.Datos.Repositories
+{
+    public interface IFacturaAlicuotaRepository : IRepository<FacturaAlicuota>
+    {
+
+    }
+
+    public class FacturaAlicuotaRepository : IFacturaAlicuotaRepository
+    {
+
+        protected readonly IDbConnection _db;
+
+        public FacturaAlicuotaRepository()
+        {
+            try
+            {
+                _db = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ToString());
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public IEnumerable<FacturaAlicuota> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Add(FacturaAlicuota entity)
+        {
+            string query = "INSERT INTO FACTURASALICUOTAS (FACTURA_ID, ALICUOTA_ID, BASE_IMPONIBLE, IMPORTE) " +
+                "VALUES (@factura_id, @alicuota_id, @base_imponible, @importe)";
+
+            try
+            {
+                using (IDbConnection _db = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ToString()))
+                {
+                    return _db.Execute(query, new { 
+                        factura_id = entity.factura_id, 
+                        alicuota_id = entity.alicuota_id, 
+                        base_imponible = entity.base_imponible, 
+                        importe = entity.importe });
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public int Modify(FacturaAlicuota entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FacturaAlicuota FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gestion.Negocio/FacturasAlicuotas.cs
@@ -0,0 +1,29 @@
+﻿using Gestion.Datos.Repositories;
+using Gestion.Entidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion.Negocio
+{
+    public class FacturasAlicuotas
+    {
+
+        public static int Add(FacturaAlicuota entity)
+        {
+            try
+            {
+                IFacturaAlicuotaRepository _repository = new FacturaAlicuotaRepository();
+                return _repository.Add(entity);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+
+    }
+}
